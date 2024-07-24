f6f89414a54c0095e20ced26bc607d6a94e2b5f6
Elvis Feda <elvisfeda123@gmail.com>
2024-07-24 15:20:54 +0000

    second commit

--- src/components/SimpleCalculator.vue
+++ src/components/SimpleCalculator.vue
@@ -16,7 +16,7 @@ const calculate = () =>{
     try {
         display.value = eval(display.value)
     } catch (error) {
-        display.value = 'error'
+        display.value = 'syntax error'
     }
 }
 
